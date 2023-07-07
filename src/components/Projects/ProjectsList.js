0516e3d3457fec62fcb1823a3d1fa9c1b490ff55
MaiNehaHu <neha207y1a0423@mlritm.ac.in>
2023-07-07 22:28:00 +0530

    NETFLIX

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -6,8 +6,8 @@ import img4 from "../../images/TodoApp.png";
 import img5 from "../../images/Carousel.png";
 import img6 from "../../images/SwipeCard.png";
 import img7 from "../../images/Clocks.png";
-import img8 from '../../images/Drag-and-Drop-Sortable-List.png'
-
+import img8 from "../../images/Drag-and-Drop-Sortable-List.png";
+import img9 from "../../images/NETFLIX.png";
 
 export default [
   {
@@ -29,54 +29,62 @@ export default [
   {
     //2
     index: 2,
+    name: "NETFLIX clone with Mood feature",
+    href: "https://mainehahu.github.io/One-Alarm/",
+    tag: "Vanilla",
+    src: img9,
+  },
+  {
+    //3
+    index: 3,
     name: "Basic Alarm using Vanilla JS",
     href: "https://mainehahu.github.io/One-Alarm/",
     tag: "Vanilla",
     src: img2,
   },
   {
-    //3
-    index: 3,
+    //4
+    index: 4,
     name: "3D card flip using CSS Transform",
     href: "https://mainehahu.github.io/3D-Card-Flip/",
     tag: "Vanilla",
     src: img3,
   },
   {
-    //4
-    index: 4,
+    //5
+    index: 5,
     name: "Basic Tasks-To-Do-App in React",
     href: "https://mainehahu.github.io/Tasks-Todo-App/",
     tag: "React",
     src: img4,
   },
   {
-    //5
-    index: 5,
+    //6
+    index: 6,
     name: "Carousel using Vanilla JS",
     href: "https://mainehahu.github.io/JS-carousel/",
     tag: "Vanilla",
     src: img5,
   },
   {
-    //6
-    index: 6,
+    //7
+    index: 7,
     name: "Animated Swipe Cards",
     href: "https://mainehahu.github.io/Animated-Swipe-Card/",
     tag: "Vanilla",
     src: img6,
   },
   {
-    //7
-    index: 7,
+    //8
+    index: 8,
     name: "Basic Digital and Analog Clocks",
     href: "https://mainehahu.github.io/Clocks/",
     tag: "Vanilla",
     src: img7,
   },
   {
-    //8
-    index: 8,
+    //9
+    index: 9,
     name: "Drag and Drop Sortable List",
     href: "https://mainehahu.github.io/Drag-and-Drop-Sortable-List/",
     tag: "Vanilla",
